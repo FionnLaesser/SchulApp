5ec7abcb9017b5de3d15e3d9571e892e04fb9081
laesf <fionn.laesser@team-w.ch>
2026-08-10 13:28:52 +0200

    Icon added

--- SchulApp/Program.cs
+++ SchulApp/Program.cs
@@ -1,3 +1,5 @@
+using SchulApp.Properties;
+
 namespace SchulApp
 {
     internal static class Program
@@ -6,9 +8,19 @@ namespace SchulApp
         static void Main()
         {
             ApplicationConfiguration.Initialize();
+            ThemeManager.Laden(); // Load the theme settings
+            Icon? appIcon = Icon.ExtractAssociatedIcon(Application.ExecutablePath);  // Extrahiert das Symbol aus der .exe Datei.
 
-            ThemeManager.Laden();
-
+            Application.Idle += (_, _) => // Setzt das Symbol für alle offenen Forms, wenn die Anwendung im Leerlauf ist.
+            {
+                foreach (Form form in Application.OpenForms)
+                {
+                    if (appIcon != null)
+                    {
+                        form.Icon = appIcon;
+                    }
+                }
+            };
             Application.Run(new Hauptmenue());
         }
     }
