dcd0c34baa1e7ceb29eae6501952f292cc538541
laesf <fionn.laesser@team-w.ch>
2026-08-17 09:08:54 +0200

    Swagger added + Auto direct to Url

--- schulAppREST/Program.cs
+++ schulAppREST/Program.cs
@@ -1,8 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using SchulApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Datenbank
 builder.Services.AddDbContext<SchulAppContext>(options =>
     options.UseSqlServer(
         "Server=localhost;" +
@@ -12,16 +13,24 @@ builder.Services.AddDbContext<SchulAppContext>(options =>
         "TrustServerCertificate=True;"
     )
 );
+
+// Controller
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "SchulApp API V1");
+    });
 }
 
 app.UseHttpsRedirection();
