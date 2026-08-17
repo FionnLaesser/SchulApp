20b06953afe8fcd9c6178e4605af90dbd159b487
laesf <fionn.laesser@team-w.ch>
2026-08-17 11:53:43 +0200

    Start Script updated

--- start.ps1
+++ start.ps1
@@ -1,10 +1,18 @@
 $startPfad = Get-Location
 
 Set-Location $PSScriptRoot
-$apiProcess = Start-Process powershell -PassThru -ArgumentList `
+
+# REST API starten
+$restProcess = Start-Process powershell -PassThru -ArgumentList `
     "-NoExit", `
     "-Command", "Set-Location '$PSScriptRoot\schulAppREST'; dotnet run"
 
+# SOAP API starten
+$soapProcess = Start-Process powershell -PassThru -ArgumentList `
+    "-NoExit", `
+    "-Command", "Set-Location '$PSScriptRoot\SchulappSOAP'; dotnet run"
+
+# Auf REST API warten
 Write-Host "Warte auf REST API..."
 
 do {
@@ -15,27 +23,60 @@ do {
             "http://localhost:63636/api/PingPong/bestenliste" `
             -UseBasicParsing
 
-        $apiBereit = $response.StatusCode -eq 200
+        $restBereit = $response.StatusCode -eq 200
     }
     catch {
-        $apiBereit = $false
+        $restBereit = $false
     }
 
-} until ($apiBereit)
+} until ($restBereit)
 
 Write-Host "REST API ist bereit."
 
+
+# Auf SOAP API warten
+Write-Host "Warte auf SOAP API..."
+
+do {
+    Start-Sleep -Seconds 1
+
+    try {
+        $response = Invoke-WebRequest `
+            "http://localhost:5210/SchuelerService.svc?wsdl" `
+            -UseBasicParsing
+
+        $soapBereit = $response.StatusCode -eq 200
+    }
+    catch {
+        $soapBereit = $false
+    }
+
+} until ($soapBereit)
+
+Write-Host "SOAP API ist bereit."
+
+
+# SchulApp starten
 Set-Location "$PSScriptRoot\SchulApp"
 
 try {
     dotnet run --project SchulApp.csproj
 }
 finally {
-    if (!$apiProcess.HasExited) {
-        taskkill /PID $apiProcess.Id /T /F | Out-Null
+
+    # REST API beenden
+    if (!$restProcess.HasExited) {
+        taskkill /PID $restProcess.Id /T /F | Out-Null
     }
+
+    # SOAP API beenden
+    if (!$soapProcess.HasExited) {
+        taskkill /PID $soapProcess.Id /T /F | Out-Null
+    }
+
     Set-Location $startPfad
 
     Write-Host "REST API wurde beendet."
-    Write-Host "Zuruek zu: $startPfad"
+    Write-Host "SOAP API wurde beendet."
+    Write-Host "Zurueck zu: $startPfad"
 }
