ee884fa62218342796d97214ab32b068248d2de3
laesf <fionn.laesser@team-w.ch>
2026-08-18 11:44:21 +0200

    feat: add PingPong back button and prevent button focus

---
+++ SchulApp/NoFocusButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace SchulApp
+{
+    public class NoFocusButton : Button
+    {
+        public NoFocusButton()
+        {
+            SetStyle(ControlStyles.Selectable, false);
+            TabStop = false;
+        }
+
+        protected override bool ShowFocusCues => false;
+    }
+}
