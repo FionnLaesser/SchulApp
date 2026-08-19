8573b97d3187e0d6810557c5b552d6e05aed90e6
Fionn Lässer <135864773+FionnLaesser@users.noreply.github.com>
2026-08-19 11:04:22 +0200

    Register REST audit logging service

--- schulAppREST/Program.cs
+++ schulAppREST/Program.cs
@@ -3,28 +3,21 @@ using Prometheus;
 using SchulApp.Data;
 using SchulAppREST.Exceptions;
 using SchulAppREST.Monitoring;
+using schulAppREST.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Datenbank
 builder.Services.AddDbContext<SchulAppContext>(options =>
     options.UseSqlServer(
-        "Server=localhost;" +
-        "Database=SchulAppDB;" +
-        "Integrated Security=True;" +
-        "Encrypt=True;" +
-        "TrustServerCertificate=True;"
+        "Server=localhost;Database=SchulAppDB;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;"
     )
 );
 
-// Datenbankstatus regelmässig als Prometheus-Metrik erfassen
 builder.Services.AddHostedService<DatabaseMetricsService>();
-
-// Controller
 builder.Services.AddControllers();
 builder.Services.AddHttpClient();
-
-// Swagger
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<AuditLogService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -33,7 +26,6 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "SchulApp API V1");
@@ -41,18 +33,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseRouting();
-
-// Unerwartete API-Fehler zentral behandeln und als einheitliche JSON-Antwort zurückgeben.
 app.UseMiddleware<GlobalExceptionMiddleware>();
-
-// /metrics muss über HTTP erreichbar bleiben, damit Prometheus aus Docker scrapen kann.
 app.UseWhen(
     context => !context.Request.Path.StartsWithSegments("/metrics"),
     branch => branch.UseHttpsRedirection()
 );
-
-// Requests, Antwortzeiten und HTTP-Statuscodes für Prometheus erfassen.
-// Der /metrics-Endpunkt selbst wird ausgeschlossen, damit die Scrapes die Request-Zahlen nicht verfälschen.
 app.UseWhen(
     context => !context.Request.Path.StartsWithSegments("/metrics"),
     branch => branch.UseHttpMetrics(options =>
@@ -60,10 +45,7 @@ app.UseWhen(
         options.ReduceStatusCodeCardinality();
     })
 );
-
 app.UseAuthorization();
-
 app.MapControllers();
 app.MapMetrics();
-
 app.Run();
