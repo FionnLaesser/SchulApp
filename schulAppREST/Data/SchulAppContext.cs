43641d49478bfcd9d1b95d74e95496fb50d23c0c
Fionn Lässer <135864773+FionnLaesser@users.noreply.github.com>
2026-08-19 11:04:03 +0200

    Expose audit log in REST context

--- schulAppREST/Data/SchulAppContext.cs
+++ schulAppREST/Data/SchulAppContext.cs
@@ -5,52 +5,27 @@ namespace SchulApp.Data
 {
     public class SchulAppContext : DbContext
     {
-        public SchulAppContext()
-        {
-        }
+        public SchulAppContext() { }
 
         public SchulAppContext(DbContextOptions<SchulAppContext> options)
-            : base(options)
-        {
-        }
+            : base(options) { }
 
-        // Tabelle dbo.Klasse
         public DbSet<KlasseModel> Klasse { get; set; }
-
-        // Tabelle dbo.Lehrer
         public DbSet<LehrerModel> Lehrer { get; set; }
-
-        // Tabelle dbo.Schueler
         public DbSet<SchuelerModel> Schueler { get; set; }
-
-        // Tabelle dbo.LehrerInformationen
         public DbSet<LehrerInformationModel> LehrerInformationen { get; set; }
-
-        // Tabelle dbo.Einstellung
-        public DbSet<EinstellungModel> Einstellung {  get; set; }
-
-        // Tabelle dbo.Kurs
+        public DbSet<EinstellungModel> Einstellung { get; set; }
         public DbSet<KursModel> Kurs { get; set; }
-
-        // Tabelle dbo.Login
-        //  public DbSet<LoginBenutzer> Login { get; set; }
-
-        // Tabelle dbo.Stundenplan
         public DbSet<StundenplanModel> Stundenplan { get; set; }
-
-        // Tabelle dbo.Benutzer
         public DbSet<LoginBenutzer> Benutzer { get; set; }
-
-        // Tabelle dbo.PingPongSpiel
         public DbSet<PingPongSpiel> PingPongSpiel { get; set; }
+        public DbSet<AuditLogModel> AuditLogs { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-            modelBuilder.ApplyConfigurationsFromAssembly(
-                typeof(SchulAppContext).Assembly
-            );
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(SchulAppContext).Assembly);
+            modelBuilder.Entity<AuditLogModel>().HasIndex(x => x.TimestampUtc);
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -58,11 +33,7 @@ namespace SchulApp.Data
             if (!optionsBuilder.IsConfigured)
             {
                 optionsBuilder.UseSqlServer(
-                    "Server=localhost;" +
-                    "Database=SchulAppDB;" +
-                    "Integrated Security=True;" +
-                    "Encrypt=True;" +
-                    "TrustServerCertificate=True;"
+                    "Server=localhost;Database=SchulAppDB;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;"
                 );
             }
         }
